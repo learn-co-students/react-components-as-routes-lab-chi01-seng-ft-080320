eac08a5b3cc13e9693d3afb38433e94410ef1a1d
KRStoddard <kimberlynrstoddard@gmail.com>
2020-10-11 14:51:30 -0500

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import { movies } from '../data';
 
+
 const Movies = () => {
+  console.log('hit')
+  const renderGenres = genres => {
+    return genres.map(genre => {
+      return <li>{genre}</li>
+    })
+  }
+  const renderMovies = () => {
+    return movies.map(movie => {
+      return <div>
+        <h2>{movie.title}</h2>
+        <p>Time: {movie.time} minutes</p>
+        <ul>Genres: {renderGenres(movie.genres)}
+        </ul>
+      </div>
+    })
+  }
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+        {renderMovies()}
     </div>
   );
 };
